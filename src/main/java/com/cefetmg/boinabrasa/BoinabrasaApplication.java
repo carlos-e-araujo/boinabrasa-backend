8a2c4e5cfd0e0583da33239c137e0148e8e17b09
Carlos E. Araújo <110763014+carlos-e-araujo@users.noreply.github.com>
2026-06-05 21:10:13 -0300

    initial commit

---
+++ src/main/java/com/cefetmg/boinabrasa/BoinabrasaApplication.java
@@ -0,0 +1,13 @@
+package com.cefetmg.boinabrasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoinabrasaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoinabrasaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cefetmg/boinabrasa/BoinabrasaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cefetmg.boinabrasa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoinabrasaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
